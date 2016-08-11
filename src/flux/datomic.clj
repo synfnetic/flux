7da0d7a14731abebe43d602eeb947b480e72389f
Anthony D'Ambrosio <adambrosio@thenavisway.com>
2016-08-11 15:20:39 -0700

    adding some basic docstrings

--- src/flux/datomic.clj
+++ src/flux/datomic.clj
@@ -13,10 +13,9 @@
 
 (defn transact!
   "Given a db connection and a vector of data formatted for a datomic transaction, replaces client temp-ids
-  with datomic temp-ids, and returns a map of client temp-ids to the real datomic ids created after the transaction
-  is run.
-  CANNOT BE RUN on a transaction that includes datomic entities, only plain data!
-  "
+  with datomic temp-ids, and returns what d/transact returns except with :tempids as a remapping of client temp-ids
+  to the real datomic ids created after the transaction is run.
+  CANNOT BE RUN on a transaction that includes datomic entities, only plain data!"
   ([database & txs]
    (let [conn (:connection database)
          om-tempids->datomic-tempids (atom {})
@@ -104,42 +103,67 @@
     (reduce (gen-tx-builder ctx)
             [] (dissoc params :db/id))))
 
-(defn add-tx "wip" [env params] (params->tx env params :action.type/add))
-(defn set-tx "wip" [env params] (params->tx env params :action.type/set))
+(defn set-tx
+  "Creates a transaction best suited for transact! but can/should work with d/transact
+   by unraveling the params by key value pairs, where the keys must be in the schema.
+   It must also have a top level :db/id that is the entity we will be modifying.
+   Ex: (set-tx env {:db/id 123 :localized-string/value \"hello world\"})
+   => [[:db.fn/cas 123 \"old string\" \"hello world\"]]"
+  [env params]
+  (params->tx env params :action.type/set))
+(defn add-tx
+  "Creates a transaction best suited for transact! but can/should work with d/transact
+   by unraveling the params by key value pairs, where the keys must be in the schema.
+   It must also have a top level :db/id that is the entity we will be modifying.
+   Unlike set-tx, add-tx will conj/concat values if it encounters an attribute with cardinality many
+   Ex: (add-tx env {:db/id 123 :artifact/display-title [{:db/id 345 :localized-string/value \"hello world\"}]})
+   => [{:db/id 345 :localized-string/value \"hello world\"}, [:refcas 123 [111] [111 345]]"
+  [env params]
+  (params->tx env params :action.type/add))
 
-(defn delete-entity-tx [_ params]
+(defn delete-entity-tx
+  "Builds a transaction that :db.fn/retractEntity on :db/id of the params,
+   see datomic's docs for more info..."
+  [_ params]
   [[:db.fn/retractEntity (:db/id params)]])
 
-(defn create-tx [_ params]
+(defn create-tx
+  "Creates a transaction from params, where the :db/id must not be an existing entity id."
+  [_ params]
   {:pre [(not (number? (:db/id params)))]}
   [params])
 
-(defn- retract-tx
-  "Only build a retract statement for existing values."
-  [{:keys [db/id] :as old-data} k]
-  (if-let [v (get old-data k)]
-    (if (or (set? v) (sequential? v))
-      (for [old-v v]
-        [:db/retract id k (cond->> old-v (:db/id old-v) :db/id
-                            (or (set? old-v) (sequential? old-v)) (mapv :db/id))])
-      [[:db/retract id k (cond-> v (:db/id v) :db/id)]])
-    []))
-
-(defn replace-tx [database {:keys [db/id] :as params}]
-  (let [conn (:connection database)
-        db (d/db conn)
-        old-data (d/touch (d/entity db id))
-        new-data (dissoc params :db/id)]
-    (->> (clojure.data/diff (set (keys old-data)) (set (keys new-data)))
-      (zipmap [:old :new :both])
-      (reduce
-        (fn [tx [tx-type diffs]]
-          (reduce (fn [tx k]
-                    (case tx-type
-                      :old (concat tx (retract-tx old-data k))
-                      ;; HACKY/SMELLY
-                      ((gen-tx-builder {:db db :id id :tx-type :action.type/set
-                                        :schema (group-by :db/ident (:schema database))})
-                       tx [k (new-data k)])))
-                  tx diffs))
-        []))))
+(letfn [(retract-tx
+          ;"Only build a retract statement for existing values."
+          [{:keys [db/id] :as old-data} k]
+          (if-let [v (get old-data k)]
+            (if (or (set? v) (sequential? v))
+              (for [old-v v]
+                [:db/retract id k (cond->> old-v (:db/id old-v) :db/id
+                                    (or (set? old-v) (sequential? old-v)) (mapv :db/id))])
+              [[:db/retract id k (cond-> v (:db/id v) :db/id)]])
+            []))]
+  (defn replace-tx
+    "Builds a transaction that 'replaces' all the values an entity had with the ones supplied in params.
+     In other words, retracts everything it had, and installs params as that new entity.
+     Has the same semantics as set-tx for now.
+     Eg: (replace-tx env {:db/id 111 :foo/bar :foo.bar/new})
+     => [[:db/retract 111 :fizz/buzz 222], [:db/cas 111 :foo/bar :foo.bar/old :foo.bar/new]]"
+    [database {:keys [db/id] :as params}]
+    (let [conn (:connection database)
+          db (d/db conn)
+          old-data (d/touch (d/entity db id))
+          new-data (dissoc params :db/id)]
+      (->> (clojure.data/diff (set (keys old-data)) (set (keys new-data)))
+        (zipmap [:old :new :both])
+        (reduce
+          (fn [tx [tx-type diffs]]
+            (reduce (fn [tx k]
+                      (case tx-type
+                        :old (concat tx (retract-tx old-data k))
+                        ;; HACKY/SMELLY
+                        ((gen-tx-builder {:db db :id id :tx-type :action.type/set
+                                          :schema (group-by :db/ident (:schema database))})
+                         tx [k (new-data k)])))
+                    tx diffs))
+          [])))))
